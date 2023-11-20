8496e8ca45326682f738c81d06b2442b6ecae106
UnSamuel <60000867swim@gmail.com>
2023-11-20 18:05:53 -0400

    subiendo estructura backend

---
+++ Backend/ProjectCars/ProjectCars/Context/AplicacionContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectCars.Models;
+
+namespace ProjectCars.Context
+{
+    public class AplicacionContext : DbContext
+
+    {
+        public AplicacionContext(DbContextOptions<AplicacionContext> options)
+            : base(options)
+        { 
+
+        }
+        public DbSet<Anuncios> Anuncios { get; set; }
+        public DbSet<Articulosblog> Articulosblog { get; set; }
+        public DbSet<Resenias> Resenias { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Vehiculos> Vehiculos { get; set; }
+
+
+
+    }
+}
---
+++ Backend/ProjectCars/ProjectCars/Models/Anuncios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCars.Models
+{
+    public class Anuncios
+    {
+        [Key]
+        public int id_anuncios { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+
+        public int id_vehiculo { get; set; }
+        public int id_usuario { get; set; }
+    }
+}
