cbcb0652d50612a49c3cb17cbc00507db2888bac
Sergio <sucapucasergio2.com>
2023-11-20 22:53:57 -0400

    subiendo Modelos

---
+++ Backend/ProjectCars/ProjectCars/Models/Resenias.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCars.Models
+{
+    public class Resenias
+    {
+        [Key]
+        public int id_resenias { get; set; }
+        public string comentario { get; set; }
+        public string calificacion { get; set; }
+
+        public int id_vendedor { get; set; }
+        public int id_comprador { get; set; }
+
+    }
+}
